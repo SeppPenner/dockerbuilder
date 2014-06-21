a7f6f499f2b79a5c1a784f8783e9c17cf981dcd0
Orne Brocaar <info@brocaar.com>
2014-06-21 20:54:54 +0200

    Wiring everything up.

--- dockerbuilder.go
+++ dockerbuilder.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"github.com/brocaar/dockerbuilder/config"
+	"github.com/brocaar/dockerbuilder/worker"
 	"github.com/brocaar/dockerbuilder/workspace"
 	"log"
+	"net/http"
 )
 
 func main() {
+	// get configuration from environment variables
 	config, err := config.GetConfiguration()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	workspace.Prepare(config)
+	// setup and prepare the workspace
+	workspace.SetConfig(config)
+	workspace.Prepare()
+
+	// make the task queue
+	var taskQueue = make(chan *worker.WorkerTask, config.NumWorkers)
+
+	// start worker processes
+	log.Printf("starting %d workers\n", config.NumWorkers)
+	for i := 0; i < config.NumWorkers; i++ {
+		go worker.Worker(taskQueue)
+	}
+
+	http.ListenAndServe(config.BindAddress, nil)
 }
