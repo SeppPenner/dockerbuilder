7848f9e777e2921f2cd96571972a16a926f3e757
Orne Brocaar <info@brocaar.com>
2014-06-24 21:17:30 +0200

    Implement functions to test the GitHub request signature.

--- handler/handler.go
+++ handler/handler.go
@@ -2,12 +2,18 @@
 package handler
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
 	"github.com/brocaar/dockerbuilder/worker"
+	"hash"
 	"net/http"
+	"strings"
 )
 
 type GitHubHandler struct {
 	taskQueue worker.TaskQueue
+	secret    []byte
 }
 
 // NewGitHubHandler returns a new instance of GitHubHandler.
@@ -22,5 +28,32 @@ func (h *GitHubHandler) Hook(w http.ResponseWriter, r *http.Request) {
 	// github ping
 	if r.Header.Get("X-Github-Event") == "ping" {
 		w.WriteHeader(http.StatusOK)
+		return
 	}
+}
+
+func getMacString(hashAlgo func() hash.Hash, key, message []byte) string {
+	mac := hmac.New(hashAlgo, key)
+	mac.Write(message)
+	macBytes := mac.Sum(nil)
+	return hex.EncodeToString(macBytes)
+}
+
+func checkGitHubMac(key []byte, gitHubSignature string, message []byte) bool {
+	signatureParts := strings.Split(gitHubSignature, "=")
+	var algo func() hash.Hash
+
+	// more algorithms can be added later if needed
+	if signatureParts[0] == "sha1" {
+		algo = sha1.New
+	} else {
+		return false
+	}
+
+	expectedMac := getMacString(algo, key, message)
+	if expectedMac == signatureParts[1] {
+		return true
+	}
+
+	return false
 }
--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,10 +1,66 @@
 package handler
 
 import (
+	"crypto/sha1"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+func TestGetMacString(t *testing.T) {
+	mac := getMacString(sha1.New, []byte("verysecret"), []byte("hello world"))
+	if mac != "3c24d84f47692d0b4aef2a5002e0bb6beb25dc75" {
+		t.Errorf("expected: 3c24d84f47692d0b4aef2a5002e0bb6beb25dc75, got: %s", mac)
+	}
+}
+
+func TestCheckGitHubMac(t *testing.T) {
+	testTable := []struct {
+		secret    string
+		signature string
+		body      string
+		expected  bool
+	}{
+		{
+			// valid
+			"verysecret",
+			"sha1=3c24d84f47692d0b4aef2a5002e0bb6beb25dc75",
+			"hello world",
+			true,
+		}, {
+			// body invalid
+			"verysecret",
+			"sha1=3c24d84f47692d0b4aef2a5002e0bb6beb25dc75",
+			"Hello World!",
+			false,
+		}, {
+			// token invalid
+			"verysecret",
+			"sha1=3c24d84f47692d0b4aef2a5002e0bb6beb25dc76",
+			"hello world",
+			false,
+		}, {
+			// algorithm invalid
+			"verysecret",
+			"sha512=3c24d84f47692d0b4aef2a5002e0bb6beb25dc75",
+			"hello world",
+			false,
+		}, {
+			// secret invalid
+			"VerySecret",
+			"sha1=3c24d84f47692d0b4aef2a5002e0bb6beb25dc75",
+			"hello world",
+			true,
+		},
+	}
+
+	for i, test := range testTable {
+		testResult := checkGitHubMac([]byte(test.secret), test.signature, []byte(test.body))
+		if testResult != test.expected {
+			t.Errorf("test %d: expected: %t, got: %t", i, test.expected, testResult)
+		}
+	}
+}
 
 // Test GitHub ping event returns 200.
 func TestGitHubHandlerPing(t *testing.T) {
